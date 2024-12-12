4200acf15281c7c85f21d78a9c4bc18320f64c0d
lijinliang <lijl@qnzsai.com>
2024-12-12 10:42:24 +0800

    8 commit

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     // 在每次构建时清理输出目录
     clean: true,
     // 设置为项目的根路径
-    publicPath: '/',
+    // publicPath: '/',
   },
   resolve: {
     // 允许省略这些扩展名的文件导入，如 import App from './App'; 自动匹配 .tsx、.ts
